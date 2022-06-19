cd4e613fe6d2dc2327c8ed170fa7552b25adaa0e
Sunday A. Jimoh <31360475+sunday4me@users.noreply.github.com>
2022-06-19 15:29:24 +0100

    printing alphabet in lowercase using a while loop

---
+++ alpha_2_lc_while#include <stdio.h>  int main(void) {/* Declare a variable and initialize it with the first member of         the range [a - z]      */char alphabet = 'a';/alpha_2_lc_while_loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* Declare a variable and initialize it with the first member of 
+       the range [a - z] 
+    */
+
+    char alphabet = 'a';
+
+     /* check to see if the current value of your variable is less than 
+         or the same as the last member (z) of the given range. While 
+         the value is less, go ahead and print the value 
+      */
+
+    while (alphabet <= 'z')
+    {
+        // print the value of the variable 
+        putchar(alphabet);
+
+        // increment the variable (ie: a becomes b)
+        alphabet++;
+    }
+
+    putchar('\n');
+    return (0);
+}
