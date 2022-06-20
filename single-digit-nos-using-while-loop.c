65fe88e2cb49aa0144c661ec0602b6556c5df3fc
Sunday A. Jimoh <31360475+sunday4me@users.noreply.github.com>
2022-06-20 13:42:00 +0100

    while loop

---
+++ single-digit-nos-using-while-loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 48;
+
+    while (x < 58)
+    {
+        putchar(x);
+        if (x < 57)
+        {
+            putchar(44);
+            putchar(32);
+        }
+        x++;
+    }
+    putchar('\n');
+
+    return (0);
+}
