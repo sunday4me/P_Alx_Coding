d7e07653f028ad29b968d69119fc59060784bb96
Sunday A. Jimoh <31360475+sunday4me@users.noreply.github.com>
2022-07-06 19:42:35 +0100

    simple code in c about functional pointer

---
+++ function_pointer.c
@@ -0,0 +1,15 @@
+// function Pointers in C/C++
+
+#include<stdio.h>
+int Add(int a, int b)
+{
+return a+b;
+}
+int main()
+{
+int c;
+int (*p)(int,int);
+p = &Add;
+c = (*p)(2,3);   //de-referencing and executing the function.
+printf("%d", c);
+}
