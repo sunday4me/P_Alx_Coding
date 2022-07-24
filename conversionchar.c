d01d78239082d7306f6e59b506f9e00c039be77a
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2022-07-24 10:58:58 +0100

    Create conversionchar.c

---
+++ conversionchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+printf("How many dogs do you have ?: ");
+double dogs;
+
+scanf("%lf", &dogs);
+
+printf("%f\n%e\n%g\n", dogs, dogs, dogs);
+
+/* Conversion Characters
+--------------------------------
+
+%f -  Decimal notation
+%e - Scientific notation
+%g - Computer decides
+
+*/
+
+return 0;
+}
